a2aed899e3b9460806e4d78ee4bce17700104421
virendraxd <shakyaraju605@gmail.com>
2026-06-05 18:57:46 +0530

    styles fix

--- frontend/src/components/institute/Nav.jsx
+++ frontend/src/components/institute/Nav.jsx
@@ -3,19 +3,20 @@ import React from 'react'
 function Nav() {
   return (
     <div>
+      <section id="institute-nav">
         <nav>
-            <div>logo</div>
-            <ul>
-                <li>About</li>
-                <li>Courses</li>
-                <li>Results</li>
-                <li>Contact</li>
-                <li><button className="btn">Admissions</button></li>
-            </ul>
+          <div>logo</div>
+          <ul>
+            <li>About</li>
+            <li>Courses</li>
+            <li>Results</li>
+            <li>Contact</li>
+            <li><button className="btn">Admissions</button></li>
+          </ul>
         </nav>
+      </section>
     </div>
   )
 }
 
 export default Nav
-    
